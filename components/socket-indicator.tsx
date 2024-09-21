20271b7c59325618a00d7bd3df3945d4e3fe33ca
Augusto <eric72001@hotmail.com>
2024-09-21 13:19:49 -0300

    refactor: :recycle: socket indicator dynamic label

--- components/socket-indicator.tsx
+++ components/socket-indicator.tsx
@@ -2,41 +2,27 @@
 
 import { useSocket } from "@/components/providers/socket-provider";
 import { Badge } from "./ui/badge";
+import { cn } from "@/lib/utils";
 
 export const SocketIndicator = () => {
     const { isConnected } = useSocket();
+    const label = isConnected ? "Live: Real-time updates" : "Fallback: Polling every 1s";
+    const abbr = isConnected ? "Live" : "Fallback"; 
 
-    if(!isConnected){
         return ( 
-        <Badge variant="outline" className="bg-yellow-600 text-white border-none">
-                        <span className="hidden sm:visible">
-
-                        Fallback: Polling every 1s
-                        </span>
-            <span className="sm:hidden">
-
-            Polling
-            </span>
-        </Badge> 
-        );
-    }
-
-    if(isConnected){
-        return ( 
-        <Badge variant="outline" className="bg-emerald-600 px-0 size-5 animate-pulse xsm:animate-none xsm:size-fit xsm:px-2.5 text-white border-none">
+        <Badge variant="outline" className={cn("bg-emerald-600 px-0 size-5 animate-pulse xsm:animate-none xsm:size-fit xsm:px-2.5 text-white border-none", !isConnected && "bg-yellow-600")}>
             <div className="hidden xsm:block">
                 <span className="hidden sm:block">
 
-                Live: Real-time updates
+                {label}                
                 </span>
                 <span className="sm:hidden">
 
-                Live
+                {abbr}
                 </span>
             </div>
         </Badge> 
         );
-    }
 }
  
 export default SocketIndicator;
