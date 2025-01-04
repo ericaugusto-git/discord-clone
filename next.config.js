889ed82a72cd0e14ac0186487a71c4751fbc76bb
Eric Augusto <eric72001@hotmail.com>
2025-01-04 04:20:43 -0300

    allow kashi os iframe

--- next.config.js
+++ next.config.js
@@ -3,51 +3,47 @@ const nextConfig = {
     reactStrictMode: false,
     productionBrowserSourceMaps: false,
     async headers() {
-        return [
-          {
-            source: '/:path*',
-            headers: [
-              {
-                key: 'X-Frame-Options',
-                value: 'ALLOW-FROM https://kashi-os.pages.dev',
-              },
-              {
-                key: 'Content-Security-Policy',
-                value: "frame-ancestors 'self' https://kashi-os.pages.dev",
-              },
-            ],
-          },
-        ]
-      },
+      return [
+        {
+          source: "/(.*)", // Apply to all routes
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: "frame-ancestors 'self' https://kashi-os.pages.dev;",
+            },
+          ],
+        },
+      ];
+    },
     async redirects() {
-        return [
-          // Basic redirect
-          {
-            source: '/',
-            destination: '/directs',
-            permanent: true,
-          },
-        ]
+      return [
+        {
+          source: "/",
+          destination: "/directs",
+          permanent: true,
+        },
+      ];
     },
     images: {
-        remotePatterns: [
-            {
-            protocol: 'https',
-            hostname: 'uploadthing.com',
-            pathname: '**'
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "uploadthing.com",
+          pathname: "**",
         },
-            {
-            protocol: 'https',
-            hostname: 'img.clerk.com',
-            pathname: '**'
+        {
+          protocol: "https",
+          hostname: "img.clerk.com",
+          pathname: "**",
         },
-            {
-            protocol: 'https',
-            hostname: 'utfs.io',
-            pathname: '**'
-        }
-    ]
-    }
-}
-
-module.exports = nextConfig
+        {
+          protocol: "https",
+          hostname: "utfs.io",
+          pathname: "**",
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
