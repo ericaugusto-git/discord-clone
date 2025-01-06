a2358610e05d88da146c7138a7a353639264d1e4
Eric Augusto <eric72001@hotmail.com>
2025-01-05 21:22:05 -0300

    fix: :bug: make current user client component

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -52,7 +52,6 @@ const FileUpload = ({onChange, value, endpoint}: FIleUploadProps) => {
     return ( 
     <UploadDropzone
     endpoint={endpoint}
-    config={{mode: 'auto'}}
     onClientUploadComplete={(res) => {
         console.log(res?.[0].url);
         onChange(res?.[0].url)
